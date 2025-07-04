563516512fd68d09768356d6217024488b9b0bd2
DiogoCarapito <diogo.carapito@icloud.com>
2025-07-04 21:58:27 +0100

    github actions correction

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,21 @@
 # pylint: disable=no-name-in-module
 
 import os
+import sys
+import pytest
 
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtCore import QTimer
 
 
 def test_window_creation():
-    # Set the QT_QPA_PLATFORM environment variable to 'offscreen' if running in CI
-    if os.environ.get("CI"):
+    # Skip this test if running on GitHub Actions and we can't set up the environment properly
+    if os.environ.get("GITHUB_ACTIONS") and sys.platform.startswith('linux'):
+        # Set the QT_QPA_PLATFORM environment variable to 'offscreen'
         os.environ["QT_QPA_PLATFORM"] = "offscreen"
+        # Also set XDG_RUNTIME_DIR which might be needed
+        if "XDG_RUNTIME_DIR" not in os.environ:
+            os.environ["XDG_RUNTIME_DIR"] = "/tmp/runtime-runner"
 
     app = QApplication([])
     app.setQuitOnLastWindowClosed(False)
